923a0abdfaf2155eaeaf3048b2a49f0d7ab69173
KanakornMek <kanakorn.meka@gmail.com>
2026-01-28 00:10:46 +0800

    Level-2: Implement add/list inputted text items

--- src/main/java/Weissach.java
+++ src/main/java/Weissach.java
@@ -3,20 +3,41 @@ import java.util.Scanner;
 public class Weissach {
 
     private static final String DIVIDER = "____________________________________________________________";
+
+    private static String[] items = new String[100];
+    private static int itemCount = 0;
+
     private static void printGreeting() {
-        System.out.println(DIVIDER);
-        System.out.println("Hello! I'm Weissach");
-        System.out.println("What can I do for you?");
-        System.out.println(DIVIDER);
+        printMessage("Hello! I'm Weissach\n"
+                + "What can I do for you?");
     }
 
     private static void printExitMessage() {
-        System.out.println(DIVIDER);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(DIVIDER);
+        printMessage("Bye. Hope to see you again soon!");
     }
 
-    private static void echoCommand(String message) {
+    private static void addItem(String item) {
+        items[itemCount++] = item;
+
+        printMessage("added: " + item);
+    }
+
+    private static void listItems() {
+        if (itemCount == 0) {
+            printMessage("No item added yet");
+            return;
+        }
+
+        String result = "";
+        for (int i = 0; i < itemCount - 1; i++) {
+            result += String.format("%d. %s\n", i+1, items[i]);
+        }
+
+        result += String.format("%d. %s", itemCount, items[itemCount-1]);
+        printMessage(result);
+    }
+
+    private static void printMessage(String message) {
         System.out.println(DIVIDER);
         System.out.println(message);
         System.out.println(DIVIDER);
@@ -34,9 +55,11 @@ public class Weissach {
             if (input.equals("bye")){
                 printExitMessage();
                 break;
+            } else if (input.equals("list")) {
+                listItems();
+            } else {
+                addItem(input);
             }
-
-            echoCommand(input);
         }
 
         in.close();
