22f431adf7df57ff8bbbc1a51d7467b46c606fe1
KanakornMek <kanakorn.meka@gmail.com>
2026-01-27 23:23:39 +0800

    Level-1: Implement echo and bye command

--- src/main/java/Weissach.java
+++ src/main/java/Weissach.java
@@ -1,23 +1,44 @@
+import java.util.Scanner;
+
 public class Weissach {
 
+    private static final String DIVIDER = "____________________________________________________________";
     private static void printGreeting() {
-        System.out.println(
-                "____________________________________________________________\n" +
-                        " Hello! I'm Weissach\n" +
-                        " What can I do for you?\n" +
-                        "____________________________________________________________"
-        );
+        System.out.println(DIVIDER);
+        System.out.println("Hello! I'm Weissach");
+        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
     }
 
     private static void printExitMessage() {
-        System.out.println(
-                "Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________"
-        );
+        System.out.println(DIVIDER);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
+    }
+
+    private static void echoCommand(String message) {
+        System.out.println(DIVIDER);
+        System.out.println(message);
+        System.out.println(DIVIDER);
     }
 
     public static void main(String[] args) {
         printGreeting();
-        printExitMessage();
+
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("> ");
+            String input = in.nextLine();
+
+            if (input.equals("bye")){
+                printExitMessage();
+                break;
+            }
+
+            echoCommand(input);
+        }
+
+        in.close();
     }
 }
