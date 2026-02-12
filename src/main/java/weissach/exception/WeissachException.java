bc05a7e01ace1100af7e50962824f78251e3415d
KanakornMek <kanakorn.meka@gmail.com>
2026-02-13 00:24:49 +0800

    A-Packages: Organize classes into packages

--- src/main/java/weissach/exception/WeissachException.java
+++ src/main/java/weissach/exception/WeissachException.java
@@ -1,3 +1,5 @@
+package weissach.exception;
+
 public class WeissachException extends Exception {
     public WeissachException(String message) {
         super(message);
--- src/main/java/weissach/task/Todo.java
+++ src/main/java/weissach/task/Todo.java
@@ -1,3 +1,5 @@
+package weissach.task;
+
 public class Todo extends Task {
     public Todo(String description) {
         super(description);
